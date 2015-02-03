78d91a2a9224761c218a947bb00cc93cf8853f14
Felicia Wang <felis_3007@yahoo.com>
2015-02-03 13:13:04 -0800

    Add bootstrap for Sign In page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_credentials(
-    params[:user][:email],
-    params[:user][:password]
+    params[:session][:email],
+    params[:session][:password]
     )
 
     if user
