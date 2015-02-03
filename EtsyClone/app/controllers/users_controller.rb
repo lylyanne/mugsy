25451a86caa32e1dda15ef726bd84019acd3fc90
Felicia Wang <felis_3007@yahoo.com>
2015-02-03 11:37:34 -0800

    Implement sign in/sign out and user creation

---
+++ EtsyClone/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      redirect_to users_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def index
+    render :index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :email)
+  end
+end
---
+++ EtsyClone/app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :session_token, uniqueness: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+end
