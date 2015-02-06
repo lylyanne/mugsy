d7b6b773e8d591bbdfd0f8cede69bc788ab1568b
Felicia Wang <felis_3007@yahoo.com>
2015-02-05 16:17:41 -0800

    Add shopping cart functionality where buyer can add item, update item, and remove item from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   def show
     @order_items = current_order.order_items
+    render :show
   end
 end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,6 +4,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.new(order_item_params)
     @order.save
     session[:order_id] = @order.id
+    redirect_to cart_url
   end
 
   def update
@@ -11,6 +12,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
+    redirect_to cart_url
   end
 
   def destroy
@@ -18,6 +20,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
     @order_items = @order.order_items
+    redirect_to cart_url
   end
 
   private
