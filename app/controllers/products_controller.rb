d41c771837183cc0acf32b8da5e8018fed6247b0
Felicia Wang <felis_3007@yahoo.com>
2015-02-05 16:49:15 -0800

    Enable add to cart option only for items that doesn't belong to my own shop

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all.where.not(shop_id: current_user.shop.id)
+    @products = Product.all
     @order_item = current_order.order_items.new
   end
 end
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,8 @@
 class ShopsController < ApplicationController
   def show
     @shop = Shop.find(params[:id])
+    @products = Product.where(shop_id: params[:id])
+    @order_item = current_order.order_items.new
     render :show
   end
 
