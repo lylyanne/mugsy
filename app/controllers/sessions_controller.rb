cb7a2aeb1f8970265ba35c18a1201fc6741da58e
Felicia Wang <felis_3007@yahoo.com>
2015-02-08 01:10:13 -0800

    Add validation so that user can only edit her own shop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
     if user
       sign_in(user)
-      redirect_to products_url
+      redirect_to root_url
     else
       flash.now[:errors] = ["Invalid email or password"]
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    render :index
-  end
-
   private
   def user_params
     params.require(:user).permit(:password, :email)
